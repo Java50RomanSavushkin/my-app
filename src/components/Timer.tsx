76ea53473dc8f275d50a7b285a9618947683cb39
RomanSavushkin <romarsa82@gmail.com>
2023-03-03 12:41:25 +0200

    commit test

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -3,6 +3,7 @@ export const Timer: React.FC = () => {
     const styles: React.CSSProperties = {
         backgroundColor: "lightblue", fontSize: "2em"
     }
+    
     setTimeout(tic, 1000);
     const [time, setTime] = React.useState(new Date());
     function tic() {
