595362ef52c57282d612fbc0423b6c2c28864cb2
RomanSavushkin <romarsa82@gmail.com>
2023-03-09 01:22:53 +0200

    HM-32

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,13 @@ import './App.css';
 import { Timer } from './components/Timer';
 
 function App() {
-  return <Timer/>
+  return <div>
+    <Timer cityCountry="Toronto" />
+    <Timer cityCountry="New York" />
+    <Timer cityCountry="Yekaterinburg" />
+    <Timer cityCountry="Japan" />
+    <Timer cityCountry="Israel" />
+  </div>
 }
+
 export default App;
--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,16 +1,53 @@
-import React from "react"
-export const Timer: React.FC = () => {
+import React, { useState, useEffect } from "react";
+import timeZones from "../time-zones";
+type Props = {
+    cityCountry: string;
+}
+
+interface TimeZone {
+    name: string;
+    countryName: string;
+    mainCities: string[];
+}
+
+export const Timer: React.FC<Props> = ({ cityCountry }) => {
     const styles: React.CSSProperties = {
-        backgroundColor: "lightblue", fontSize: "2em"
+        backgroundColor: "lightblue",
+        fontSize: "2em"
+    };
+
+    function findTimeZoneByCityName(timeZones: TimeZone[], cityCountry: string) {
+        for (let i = 0; i < timeZones.length; i++) {
+            if (timeZones[i].mainCities.includes(cityCountry) || timeZones[i].countryName.includes(cityCountry)) {
+                return timeZones[i].name;
+            }
+
+            for (let city of timeZones[i].mainCities) {
+                if (city.includes(cityCountry)) {
+                    return timeZones[i].name;
+                }
+            }
+        }
+        return "Asia/Jerusalem";
     }
-    
-    setTimeout(tic, 1000);
-    const [time, setTime] = React.useState(new Date());
+
+    const [time, setTime] = useState<Date>(new Date());
+
     function tic() {
-        setTime(new Date())
+        setTime(new Date());
+
     }
+
+    useEffect(() => {
+        const interval = setInterval(tic, 2000);
+        console.log("useEffect");
+        return () => clearInterval(interval);
+    }, [])
+
+    const res: string = findTimeZoneByCityName(timeZones, cityCountry);
+
     return <div>
-        <h2>Current Time</h2>
-        <p style={styles}>{time.toLocaleTimeString()} </p>
+        <h2 >Current Time in {cityCountry}</h2>
+        <p style={styles}>{time.toLocaleTimeString(undefined, { timeZone: res })}</p>
     </div>
 }
