f368144c813ed638c855d3e3285c36469ac9c2cd
RomanSavushkin <romarsa82@gmail.com>
2023-03-19 21:36:12 +0200

    CW-#35 deleting unnecessary files

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,8 +1,8 @@
-import {configureStore} from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
 import { gameReducer } from "./gameSlice";
 
 export const store: any = configureStore({
-    reducer: {
-      gameRow: gameReducer
-    }
+  reducer: {
+    gameRow: gameReducer
+  }
 })
