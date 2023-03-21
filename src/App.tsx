61fd75967ede5b8ff6c0ab046c3522b4312d9a07
RomanSavushkin <romarsa82@gmail.com>
2023-03-21 21:48:45 +0200

    finale

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-//import { type } from 'os';
+import { type } from 'os';
 import React, { ReactNode } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import './App.css';
@@ -7,20 +7,20 @@ import { CellType } from './model/CellType';
 import { gameActions } from './redux/gameSlice';
 
 function App() {
-  const cells = useSelector<any, CellType[] | string>(state => state.gameRow.cells);
-  const dispatch = useDispatch();
-  function getRow(): ReactNode {
-    if (typeof cells == "string") {
-      return <p>Game is over</p>
+    const cells = useSelector<any, CellType[] |string>(state=>state.gameRow.cells);
+    const dispatch = useDispatch();
+    function getRow(): ReactNode {
+      if(typeof cells == "string") {
+        return <p>Game is over</p>
+      }
+          return cells.map(cell => <Cell width={'5vw'} cell={cell}
+           clickFn={function (id: number): void {
+           dispatch(gameActions.move(id))
+          } }/>)
     }
-    return cells.map(cell => <Cell width={'5vw'} cell={cell}
-      clickFn={function (id: number): void {
-        dispatch(gameActions.move(id))
-      }} />)
-  }
-  return <div style={{ display: 'flex' }}>
-    {getRow()}
-  </div>
+ return <div style={{display:'flex'}}>
+      {getRow()}
+ </div>
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -12,9 +12,9 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+       <App />
     </Provider>
-
+   
   </React.StrictMode>
 );
 
